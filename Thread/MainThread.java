9d4ad986ad4dadac4779c81a05ae823d262ecc29
aneeshvk <aneeshvken@gmail.com>
2022-03-12 14:02:25 +0530

    first commit

---
+++ Thread/MainThread.java
@@ -0,0 +1,10 @@
+class MainThread
+{
+public static void main(String args[])
+{
+ Thread t= Thread.currentThread();
+System.out.println("Current Thread "+t);
+t.setName("My Thread");
+System.out.println("Current Thread "+t);
+}
+}
